3f499137c4220fee6fc729bf251dd932b5751b1a
gnlehc <chelseang40@gmail.com>
2023-06-15 20:19:11 +0700

    Fix: Folder Route

---
+++ scripts/dataDummy.js
@@ -0,0 +1,14 @@
+var data = [
+    {
+        name: "cO.Okies",
+        type: "PASTRY"
+    },
+    {
+        name: "Aiskrimmm",
+        type: "DESSERT"
+    },
+    {
+        name: "LolliPolli",
+        type: "DESSERT"
+    }
+]
