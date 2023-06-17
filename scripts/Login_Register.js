b9b5e16821ef9213a0283dbafc9c6d213132439a
gnlehc <chelseang40@gmail.com>
2023-06-17 17:51:07 +0700

    Feat: Register Responsive

--- scripts/Login_Register.js
+++ scripts/Login_Register.js
@@ -12,11 +12,49 @@
 document.getElementById("form-field").addEventListener("submit", (event) => {
     event.preventDefault();
     var username = document.getElementById('name').value;
-    var storedName = localStorage.getItem('name');
-    var jsonArray = storedName ? JSON.parse(storedName) : [];
-    jsonArray.push(username);
-    localStorage.setItem('name', JSON.stringify(jsonArray));
+    var email = document.getElementById('email').value;
+    var noTLP = document.getElementById('tlp').value;
+    var password = document.getElementById('password').value;
+    var confirm = document.getElementById('confirm').value;
+    var error = document.getElementById("error");
+    var storedData = localStorage.getItem('userData');
+
+    var jsonArray = storedData ? JSON.parse(storedData) : []
+
+    var user = {
+        name: username,
+        email: email,
+        noTLP: noTLP,
+        password: password,
+        confirm: confirm,
+    }
+
+    if (!username || !email || !noTLP || !password || !confirm) {
+        console.log("ISI ANJIR")
+        error.textContent = "Field is required";
+        error.style.cssText = "color: red; font-size: 1vw;";
+        var inputElements = document.getElementsByTagName("input");
+        for (var i = 0; i < inputElements.length; i++) {
+            var input = inputElements[i];
+            if (!input.value) {
+                input.style.borderColor = "red";
+            }
+            else {
+                input.style.borderColor = "";
+            }
+        }
+        return
+    } else {
+        if (username.length < 4 || username.length > 15) {
+        }
+    }
+
+    jsonArray.push(user);
+    localStorage.setItem('userData', JSON.stringify(jsonArray));
     console.log(jsonArray);
+
+
+
 });
 
 
